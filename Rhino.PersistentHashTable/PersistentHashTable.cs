0d2380a70d020e5cf2a15c544d642a55b2c86c66
Piotr Bazydło <piotr.bazydlo@student.put.poznan.pl>
2014-03-06 21:51:24 +0100

    Replace OpenTableGrbit.ReadOnly flag with None so that dht works.

--- Rhino.PersistentHashTable/PersistentHashTable.cs
+++ Rhino.PersistentHashTable/PersistentHashTable.cs
@@ -100,7 +100,7 @@ namespace Rhino.PersistentHashTable
 			{
 				instance.WithDatabase(database, (session, dbid) =>
 				{
-					using (var details = new Table(session, dbid, "details", OpenTableGrbit.ReadOnly))
+					using (var details = new Table(session, dbid, "details", OpenTableGrbit.None))
 					{
 						Api.JetMove(session, details, JET_Move.First, MoveGrbit.None);
 						var columnids = Api.GetColumnDictionary(session, details);
